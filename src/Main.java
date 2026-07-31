05eefb1ccdb86696cb989b2805688769c1db4f70
Marco Cozma <marcocozma97@gmail.com>
2026-07-31 10:11:04 +0200

    Prova

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao!");
-        System.out.println("Benvenuto");
+        System.out.println("Benvenut");
     }
 }
